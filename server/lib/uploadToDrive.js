ec9b866584cd634c492c52ba2bf56e6bd55f7e4b
Tilak <tilaksasmal999@gmail.com>
2020-06-09 22:28:02 +0530

    added support for nested directories

--- server/lib/uploadToDrive.js
+++ server/lib/uploadToDrive.js
@@ -4,24 +4,28 @@ const path = require('path')
 const { getAuthClient, createFolder, uploadFile} = require('./driveLib')
 const constants = require('./constants.json')
 
+async function uploadFolder(srcPath, folderId=constants.driveMediaFolder) {
+    if(fs.lstatSync(srcPath).isDirectory()) {
+        const folderName = srcPath.split(path.sep).pop()
+        let folder = await createFolder(folderId, folderName)
+        const items = await fs.promises.readdir(srcPath);
+        for(const item of items) {
+            const itemPath = path.join(srcPath, item);
+            if(fs.lstatSync(itemPath).isDirectory())
+                await uploadFolder(itemPath, folder.data.id)
+            else
+                await uploadFile(folder.data.id, itemPath)
+        }
+    } else {
+        await uploadFile(folderId, srcPath)
+    }
+}
+
+
 module.exports = async function(torrent){
     await getAuthClient()
     const sourcefolderPath = path.join(constants.downloadPath, torrent.name)
-
-    if(fs.lstatSync(sourcefolderPath).isDirectory()) {
-        let folder = await createFolder(constants.driveMediaFolder, torrent.name)
-        console.log("folder created with id:", folder)
-        const files = await fs.promises.readdir(sourcefolderPath);
-        for( const file of files ) {
-            const fromPath = path.join( sourcefolderPath, file );
-            await uploadFile(folder.data.id, fromPath)
-            console.log("file uploaded:", file)
-        }
-    } else {
-        await uploadFile(constants.driveMediaFolder, sourcefolderPath)
-        console.log("file uploaded:", sourcefolderPath)
-    }
-
+    uploadFolder(sourcefolderPath)
     return true;
 }
 
