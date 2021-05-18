3a2735dc3586d315d286b2001f6d1db23d3d0c30
Tilak Sasmal <tilak@Tilaks-MacBook-Air.local>
2021-05-18 17:00:46 +0530

    removed gdrive upload

--- server/api/index.js
+++ server/api/index.js
@@ -1,133 +1,158 @@
-const express = require('express')
-const router = express.Router()
-const WebTorrent = require('webtorrent')
-const constant = require('../lib/constants.json')
-const uploadToDrive = require('../lib/uploadToDrive')
-const https = require('https');
-const db = require('../lib/db')
+const express = require("express");
+const router = express.Router();
+const WebTorrent = require("webtorrent");
+const constant = require("../lib/constants.json");
+const https = require("https");
+const db = require("../lib/db");
 
 router.use(express.json());
-const client = new WebTorrent()
+const client = new WebTorrent();
 
-setInterval(()=>{
-    const inProgress = client.torrents.filter((torrent)=>!torrent.done)
-    if (inProgress.length > 0) {
-        https.get('https://tornedo.herokuapp.com')
-        console.log('pinging host')
-    }
-}, 1000*60*5)
+setInterval(() => {
+  const inProgress = client.torrents.filter((torrent) => !torrent.done);
+  if (inProgress.length > 0) {
+    https.get("https://tornedo.herokuapp.com");
+    console.log("pinging host");
+  }
+}, 1000 * 60 * 5);
 
-function onReady(torrent){
-    db.insertLog({
-        torrentName: torrent.name,
-        infoHash: torrent.infoHash,
-        magnet: torrent.magnetURI,
-        status: 0
-    })
-    torrent.on('done', ()=>{
-        db.updateLog({infoHash: torrent.infoHash, status: 1})
-        uploadToDrive(torrent).then(()=>{
-            db.updateLog({infoHash: torrent.infoHash, status: 2})
-        })
-    })
+function onReady(torrent) {
+  db.insertLog({
+    torrentName: torrent.name,
+    infoHash: torrent.infoHash,
+    magnet: torrent.magnetURI,
+    status: 0,
+  });
+  torrent.on("done", () => {
+    db.updateLog({ infoHash: torrent.infoHash, status: 1 });
+  });
 }
 
 // define the home page route
-router.get('/getAllTorrents', function (req, res) {
-    const torrentList = []
-    for(const torrent of client.torrents) {
-        const { name, infoHash, downloaded, uploaded, 
-            downloadSpeed, uploadSpeed, progress, ratio, 
-            numPeers, path, ready, paused, done, length } = torrent
-        torrentList.push({ name, infoHash, downloaded, uploaded, 
-            downloadSpeed, uploadSpeed, progress, ratio,
-            numPeers, path, ready, paused, done, length })
-    }
-    res.send({ status: 'success', torrentList })
-})
+router.get("/getAllTorrents", function (req, res) {
+  const torrentList = [];
+  for (const torrent of client.torrents) {
+    const {
+      name,
+      infoHash,
+      downloaded,
+      uploaded,
+      downloadSpeed,
+      uploadSpeed,
+      progress,
+      ratio,
+      numPeers,
+      path,
+      ready,
+      paused,
+      done,
+      length,
+    } = torrent;
+    torrentList.push({
+      name,
+      infoHash,
+      downloaded,
+      uploaded,
+      downloadSpeed,
+      uploadSpeed,
+      progress,
+      ratio,
+      numPeers,
+      path,
+      ready,
+      paused,
+      done,
+      length,
+    });
+  }
+  res.send({ status: "success", torrentList });
+});
 
 // define the about route
-router.post('/addTorrent', function (req, res) {
-    const { magnet, torrentFile } = req.body
-    const torrentId = magnet || Buffer.from(torrentFile, 'base64')
-    client.add(torrentId, { path: constant.downloadPath }, onReady)
-    console.log(">> torrent added")
-    res.send({
-        status: "success"
-    })
-})
+router.post("/addTorrent", function (req, res) {
+  const { magnet, torrentFile } = req.body;
+  const torrentId = magnet || Buffer.from(torrentFile, "base64");
+  client.add(torrentId, { path: constant.downloadPath }, onReady);
+  console.log(">> torrent added");
+  res.send({
+    status: "success",
+  });
+});
 
 // List all the files in the torrent
-router.post('/getFiles', function(req, res){
-    const { infoHash } = req.body
-    const torrent = client.get(infoHash)
-    const files = []
-    for(const file of torrent.files) {
-        const { name, path, length, downloaded, progress} = file
-        files.push({ name, path, length, downloaded, progress })
-    }
-    res.send({ status: 'success', files})
-})
+router.post("/getFiles", function (req, res) {
+  const { infoHash } = req.body;
+  const torrent = client.get(infoHash);
+  const files = [];
+  for (const file of torrent.files) {
+    const { name, path, length, downloaded, progress } = file;
+    files.push({ name, path, length, downloaded, progress });
+  }
+  res.send({ status: "success", files });
+});
 
-router.post('/actionDelete', function(req, res){
-    const { infoHash } = req.body
-    const torrent = client.get(infoHash)
-    try {
-        torrent.destroy()
-        db.updateLog({infoHash: torrent.infoHash, status: 3})
-        res.send({
-            status: 'success',
-        })
-    } catch(e){
-        res.send({
-            status: 'fail',
-            message: e.toString()
-        })
-    }
-})
+router.post("/actionDelete", function (req, res) {
+  const { infoHash } = req.body;
+  const torrent = client.get(infoHash);
+  try {
+    torrent.destroy();
+    db.updateLog({ infoHash: torrent.infoHash, status: 3 });
+    res.send({
+      status: "success",
+    });
+  } catch (e) {
+    res.send({
+      status: "fail",
+      message: e.toString(),
+    });
+  }
+});
 
-router.post('/actionPause', function(req, res){
-    const { infoHash } = req.body
-    const torrent = client.get(infoHash)
-    try {
-        torrent.pause()
-        res.send({
-            status: 'success',
-        })
-    } catch(e){
-        res.send({
-            status: 'fail',
-            message: e.toString()
-        })
-    }
-})
+router.post("/actionPause", function (req, res) {
+  const { infoHash } = req.body;
+  const torrent = client.get(infoHash);
+  try {
+    torrent.pause();
+    res.send({
+      status: "success",
+    });
+  } catch (e) {
+    res.send({
+      status: "fail",
+      message: e.toString(),
+    });
+  }
+});
 
-router.post('/actionResume', function(req, res){
-    const { infoHash } = req.body
-    const torrent = client.get(infoHash)
-    try {
-        torrent.resume()
-        res.send({
-            status: 'success',
-        })
-    } catch(e){
-        res.send({
-            status: 'fail',
-            message: e.toString()
-        })
-    }
-})
+router.post("/actionResume", function (req, res) {
+  const { infoHash } = req.body;
+  const torrent = client.get(infoHash);
+  try {
+    torrent.resume();
+    res.send({
+      status: "success",
+    });
+  } catch (e) {
+    res.send({
+      status: "fail",
+      message: e.toString(),
+    });
+  }
+});
 
-router.get('/getLogs', function(req, res){
-    db.getAllLogs().then(rows=>res.send({
-        status: 'success', logs: rows
-    }))
-})
+router.get("/getLogs", function (req, res) {
+  db.getAllLogs().then((rows) =>
+    res.send({
+      status: "success",
+      logs: rows,
+    })
+  );
+});
 
-router.post('/deleteLog', function(req, res){
-    db.deleteLog(req.body.id)
-        .then(data=>res.send({ status: 'success'}))
-        .catch(err=>res.send({ status: 'fail', err }))
-})
+router.post("/deleteLog", function (req, res) {
+  db.deleteLog(req.body.id)
+    .then((data) => res.send({ status: "success" }))
+    .catch((err) => res.send({ status: "fail", err }));
+});
 
-module.exports = router
+module.exports = router;
--- src/components/Files.js
+++ src/components/Files.js
@@ -1,32 +1,40 @@
-import React, {useEffect, useState} from 'react';
-import { formatBytes, round } from '../library/helper'
-import { getFiles } from '../library/ajax'
+import React, { useEffect, useState } from "react";
+import { formatBytes, round } from "../library/helper";
+import { getFiles } from "../library/ajax";
 
 export default function Files({ infoHash }) {
-    const [files, setFiles] = useState([{ name: 'Loading..' }])
+  const [files, setFiles] = useState([{ name: "Loading.." }]);
 
-    useEffect(()=>{
-        const i = setInterval(async()=>{
-            const res = await getFiles(infoHash)
-            if (res.status === 'success')
-                setFiles(res.files)
-            else
-                console.log(res)
-        }, 1500)
-        return ()=>clearInterval(i)
-    })
+  useEffect(() => {
+    const i = setInterval(async () => {
+      const res = await getFiles(infoHash);
+      if (res.status === "success") setFiles(res.files);
+      else console.log(res);
+    }, 1500);
+    return () => clearInterval(i);
+  });
 
-    return (
-        <table className="table-auto w-full text-gray-800 text-sm">
-            <tbody>{
-                files.map((file)=><tr key={file.name}>
-                    <td className="border px-2 py-2">
-                        <a href={'/downloads/'+file.path}>{file.name}</a>
-                    </td>
-                    <td className="border px-2 py-2">{formatBytes(file.length)}</td>
-                    <td className="border px-2 py-2">{round(file.progress*100, 1)}%</td>
-                </tr>)
-            }</tbody>
-        </table>
-    )
+  return (
+    <table className="table-auto w-full text-gray-800 text-sm">
+      <tbody>
+        {files.map((file) => (
+          <tr key={file.name}>
+            <td className="border px-2 py-2">
+              <a
+                href={"/downloads/" + file.path}
+                target="_blank"
+                className="hover:text-blue-600"
+              >
+                {file.name}
+              </a>
+            </td>
+            <td className="border px-2 py-2">{formatBytes(file.length)}</td>
+            <td className="border px-2 py-2">
+              {round(file.progress * 100, 1)}%
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
