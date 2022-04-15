25aee44abf07da1e169a8c5d5a15a4a4847639a8
tilak sasmal <tilaksasmal999@gmail.com>
2022-04-15 15:54:58 +0530

    refactored code to resolve bug

--- server/lib/uploadToDrive.js
+++ server/lib/uploadToDrive.js
@@ -2,27 +2,36 @@ const GdriveFS = require("@ideabox/cloud-drive-fs").default;
 const fs = require("fs");
 const path = require("path");
 
+const uploadFile = async (filepath, name, folderId, g) => {
+  try {
+    await g.uploadFile(fs.createReadStream(filepath), {
+      name: name,
+      size: fs.statSync(filepath).size,
+      parentId: folderId,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 const uploadFolder = async (rootPath, parentId, g) => {
   console.log("Uploading:", rootPath);
-  const dirname = path.basename(rootPath);
   try {
-    const folder = await g.createFolder(dirname, parentId);
-    for (const file of fs.readdirSync(rootPath)) {
-      console.log("->", file);
-      const filepath = path.join(rootPath, file);
-      if (fs.lstatSync(filepath).isDirectory()) {
-        await uploadFolder(filepath, folder.id);
-      } else {
-        try {
-          await g.uploadFile(fs.createReadStream(filepath), {
-            name: file,
-            size: fs.statSync(filepath).size,
-            parentId: folder.id,
-          });
-        } catch (e) {
-          console.log(e);
+    if (fs.lstatSync(rootPath).isDirectory()) {
+      const dirname = path.basename(rootPath);
+      const folder = await g.createFolder(dirname, parentId);
+      for (const file of fs.readdirSync(rootPath)) {
+        console.log("->", file);
+        const filepath = path.join(rootPath, file);
+        if (fs.lstatSync(filepath).isDirectory()) {
+          await uploadFolder(filepath, folder.id);
+        } else {
+          await uploadFile(filepath, file, folder.id, g);
         }
       }
+    } else {
+      const filename = path.basename(rootPath);
+      await uploadFile(rootPath, filename, parentId, g);
     }
   } catch (e) {
     console.log(e);
